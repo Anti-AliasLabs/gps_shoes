c33246408fe11e14a3af220bae091f644fdb45c8
Becky Stewart <becky@theleadingzero.com>
2012-10-10 14:18:59 +0100

    Right shoe sketch now switches states correctly and reads in GPS data. Doesn't yet communicate wirelessly or display direction.

--- nplh/nplh.ino
+++ nplh/nplh.ino
@@ -7,49 +7,53 @@
  becky@codasign.com
  July 2012
  
- Serial port reads in GPS data (via XBee) and writes out
+ Serial port reads in GPS data and writes out
  instructions to remote XBee controlling LEDS.
  
- Serial on 0 and 1
- LEDs 
  
  
  */
 
 #include <TinyGPS.h>
-#include <Wire.h>
-#include <PinChangeInt.h>
 #include <VirtualWire.h>
-//#include <NoportcSoftwareSerial.h>
+#include <SoftwareSerial.h>
 
-#define NO_PORTA_PINCHANGES
-#define NO_PORTB_PINCHANGES
+const int gpsIn = 1;
+const int heelPin = 2;
 
+int centreLED = 9;
+int ledPins[] = {
+  6, 13, 14, 15, 16, 17, 8, 7};
 
-const int heelPin = 17;
+int ledsLen = 8;
+/*
+ N  6
+ NE 13
+ E  14
+ SE 15
+ S  16
+ SW 17
+ W  8
+ NW 7
+ */
+
+int fadeValue = 0;
+int fadeDirection = 2;
+
+unsigned long lastGPSfeed;
 
 const unsigned long timeLimit = 5000;
-const int triggerNum = 1;
-
-
-int ledPins[] = {
-  6, 7, 8, 9};
-
-int ledsLen = 4;
+const int triggerNum = 3;
 
 unsigned long initDetect;
 int currCount = 0;
 
 int goHome = 0;
 
-int HMC6352SlaveAddress = 0x42;
-int HMC6352ReadAddress = 0x41; //"A" in hex, A command is: 
 
 int headingValue;
 float startLat = 0.0;
 float startLon = 0.0;
-
-uint8_t latest_interrupted_pin;
 
 TinyGPS gps;
 
@@ -62,22 +66,18 @@ void setup()
 {
   Serial.begin(57600);
 
-  // "The Wire library uses 7 bit addresses throughout. 
-  //If you have a datasheet or sample code that uses 8 bit address, 
-  //you'll want to drop the low bit (i.e. shift the value one bit to the right), 
-  //yielding an address between 0 and 127."
-  HMC6352SlaveAddress = HMC6352SlaveAddress >> 1; // I know 0x42 is less than 127, but this is still required
-  Wire.begin();
-
   // set up interrupt for heel clicks
   pinMode(heelPin, INPUT); 
   digitalWrite(heelPin, HIGH);
-  PCintPort::attachInterrupt(heelPin, &heelClicked, RISING);
+  attachInterrupt(0, heelClicked, CHANGE);
 
   // set up LED display
   for (int i=0; i<ledsLen; i++){
     pinMode(ledPins[i], OUTPUT); 
   }
+  
+  // initialise GPS reading timer
+  lastGPSfeed = millis();
 }
 
 //---------------------------------------------------------------------------------
@@ -86,11 +86,11 @@ void setup()
 void loop()
 {
   bool newdata = false;
-  unsigned long start = millis();
+  unsigned long loopStart = millis();
 
   // if it's been longer than the time limit
   // reset the counter
-  if ( currCount > 0 && (millis()-initDetect) > timeLimit )
+  if ( currCount > 0 && (loopStart-initDetect) > timeLimit )
   {
     currCount = 0; 
     Serial.println("resetting counter");
@@ -100,23 +100,29 @@ void loop()
   if ( currCount >= triggerNum && (timeLimit-initDetect) )
   {
     // print the results to the serial monitor:
-    Serial.print("knock knock " );  
+    Serial.print("starting directions " );  
     Serial.println(currCount);
     currCount = 0;
     goHome = 1;
   }
+
+  // we are showing directions
   if ( goHome ){
     // Every second we print an update
-    while (millis() - start < 1000)
+    if ( (loopStart - lastGPSfeed) > 1000)
     {
       if (feedgps())
         newdata = true;
+       
+       lastGPSfeed = millis();
     }
+    // spin lights if we don't have a signal
+    // show directions if we do
     updateGPS(gps);
   }
-  /*else {
-   spinLights();
-   }*/
+  else {
+    standby();
+  }
 }
 
 //---------------------------------------------------------------------------------
@@ -179,37 +185,6 @@ static void updateGPS(TinyGPS &gps)
 
 
 //---------------------------------------------------------------------------------
-// readCompass - returns the current heading in degrees
-//---------------------------------------------------------------------------------
-int readCompass()
-{
-  //"Get Data. Compensate and Calculate New Heading"
-  Wire.beginTransmission(HMC6352SlaveAddress);
-  Wire.write(HMC6352ReadAddress);              // The "Get Data" command
-  Wire.endTransmission();
-  //time delays required by HMC6352 upon receipt of the command
-  //Get Data. Compensate and Calculate New Heading : 6ms
-  delay(6);
-
-  Wire.requestFrom(HMC6352SlaveAddress, 2); //get the two data bytes, MSB and LSB
-
-  //"The heading output data will be the value in tenths of degrees
-  //from zero to 3599 and provided in binary format over the two bytes."
-  byte MSB = Wire.read();
-  byte LSB = Wire.read();
-
-  float headingSum = (MSB << 8) + LSB; //(MSB / LSB sum)
-  float headingInt = headingSum / 10; 
-
-  Serial.print(headingInt);
-  Serial.println(" degrees");
-
-  //delay(100);
-  return headingInt;
-}
-
-
-//---------------------------------------------------------------------------------
 // feedgps - returns true if GPS data available
 //---------------------------------------------------------------------------------
 static bool feedgps()
@@ -226,29 +201,40 @@ static bool feedgps()
 // heelClicked - increments counter for heel clicks
 //---------------------------------------------------------------------------------
 void heelClicked() {
-  latest_interrupted_pin = PCintPort::arduinoPin;
-  // if interrupt was on pin we're watching
-  if (latest_interrupted_pin == heelPin){
-    currCount++;
+  currCount++;
 
-    // if this is the first knock
-    // note the time
-    if ( currCount == 1 )
-    {
-      initDetect = millis(); 
-    }
+  // if this is the first knock
+  // note the time
+  if ( currCount == 1 )
+  {
+    initDetect = millis(); 
   }
 }
 
+//---------------------------------------------------------------------------------
+// standby - standby power light
+//---------------------------------------------------------------------------------
+void standby(){
+  // fade centre from min to max in increments of 5 points:
+  analogWrite(centreLED, fadeValue);  
+
+  fadeValue += fadeDirection;
+  if(fadeValue > 253 || fadeValue < 2)
+    fadeDirection = -1*fadeDirection;  
+
+
+  delay(30);
+}
 
 //---------------------------------------------------------------------------------
 // spinLights - spinning light pattern
 //---------------------------------------------------------------------------------
 void spinLights()
 {
+  digitalWrite(centreLED, LOW);
   for (int i=0; i<ledsLen; i++){
     digitalWrite(ledPins[i], HIGH); 
-    delay(300);
+    delay(200);
     digitalWrite(ledPins[i], LOW); 
   }
 }
@@ -256,33 +242,14 @@ void spinLights()
 //---------------------------------------------------------------------------------
 // updateLightsHeading - update light indicating north
 //---------------------------------------------------------------------------------
-/*void updateLightsHeading(int heading)
- {
- int w = 180/ledsLen;
- int ledOn;
- 
- if ( heading > (360-w) || heading < w ){
- ledOn = 0;
- }
- else {
- float f_ledOn = float(heading-w)/(360.0-w) * ledsLen;
- ledOn = round(f_ledOn);
- }
- 
- //int ledOn = float(heading)/360.0 * ledsLen;
- Serial.println(ledOn);
- 
- for (int i=0; i<ledsLen; i++){
- digitalWrite(ledPins[i], LOW); 
- }
- digitalWrite(ledPins[ledOn], HIGH);
- }*/
-//---------------------------------------------------------------------------------
-// updateLightsHeading - update light indicating north
-//---------------------------------------------------------------------------------
 void updateLightsHeading(float c, float h)
 {
-  float w = 180/ledsLen;
+  
+  digitalWrite(ledPins[0], HIGH);
+  digitalWrite(ledPins[3], HIGH);
+  digitalWrite(ledPins[5], HIGH);
+  digitalWrite(ledPins[7], HIGH);
+  /*float w = 180/ledsLen;
   int ledOn;
 
   if ( c-h > (360-w) || c-h < w ){
@@ -297,14 +264,19 @@ void updateLightsHeading(float c, float h)
 
   //int ledOn = float(heading)/360.0 * ledsLen;
   Serial.println(ledOn);
-  
-  
+
+
 
   for (int i=0; i<ledsLen; i++){
     digitalWrite(ledPins[i], LOW); 
   }
   digitalWrite(ledPins[ledOn], HIGH);
+  */
 }
+
+
+
+
 
 
 
